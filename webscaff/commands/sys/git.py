373654394031f2f1e532896652ae930f75032880
idle sign <idlesign@yandex.ru>
2019-10-01 20:21:31 +0700

    Git clone improved.

--- webscaff/commands/sys/git.py
+++ webscaff/commands/sys/git.py
@@ -4,10 +4,11 @@ def clone(ctx, path_base, repo_url, dir_target):
 
     if 'github' in repo_url:
         # Just to make sure ssh agent forwarding works well.
+        ctx.run('ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts', warn=True)
         ctx.run('ssh -T git@github.com', warn=True)
 
     with ctx.cd(path_base):
-        ctx.run('git clone %s %s' % (repo_url, dir_target))
+        ctx.run('git clone -v %s %s' % (repo_url, dir_target))
 
 
 def pull(ctx, path_base):
