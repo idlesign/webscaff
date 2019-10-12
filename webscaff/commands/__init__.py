61e923123f59e5f1337f5d6577fa694eacdc369c
idle sign <idlesign@yandex.ru>
2019-10-12 17:20:53 +0700

    'proj' prefix replaced with 'run'

--- webscaff/commands/__init__.py
+++ webscaff/commands/__init__.py
@@ -1,10 +1,9 @@
-from ..utils import collection_from_sub
-from .proj.utils import bootstrap, rollout, backup
-from .proj.log import stream
-from .proj.uwsgi import reload_touch, maintenance_on, maintenance_off
-from .proj.service import restart, status
+from .run.log import stream
+from .run.service import restart, status
+from .run.utils import bootstrap, rollout, backup
+from .run.uwsgi import reload_touch, maintenance_on, maintenance_off
 from .sys.utils import reboot, info
-
+from ..utils import collection_from_sub
 
 ns = collection_from_sub(__file__, __name__)
 ns.add_task(backup)
