a4a1459f3832c2c099840424ab7fc8a6e2df5e50
idle sign <idlesign@yandex.ru>
2025-07-12 17:44:32 +0700

    Pin down 3.10.

--- tools/devboot.sh
+++ tools/devboot.sh
@@ -3,5 +3,5 @@
 # install makeapp from sources for local development
 cd ../
 # venv will be in current directory and in ~/.local/share/uv/tools/webscaff/
-uv tool install --force -e .
+uv tool install --force --python 3.10 -e .
 uv sync
